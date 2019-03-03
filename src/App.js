1c394cefb8298fdb80b9fa8524ab4902045530d7
Jeremy Banka <jeremybanka@gmail.com>
2019-03-02 20:05:28 -0800

    color input works

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { color: "#ff0000" };
+  }
+  updateColor = event => {
+    this.setState({ color: event.target.value });
+    console.log(event.target.value);
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p className="bigIcon">R</p>
-          <h1>Under Construction</h1>
+          <p className="bigIcon" style={{ color: this.state.color }}>
+            R
+          </p>
+          <form>
+            <label>a</label>
+            <input value={this.state.color} onChange={this.updateColor} />
+            <button>go</button>
+          </form>
         </header>
       </div>
     );
