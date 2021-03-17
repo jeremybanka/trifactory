10549851df30d87dd64f1c37fadb61a53018c373
Jeremy Banka <jeremybanka@gmail.com>
2021-03-17 01:00:08 -0700

    delete gh-pages branch and followed best practices for react deployment instead

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React from 'react'
+import React from "react"
 import {
-  BrowserRouter as Router,
+  BrowserRouter as HashRouter,
   Switch,
   Route,
   Link,
-} from 'react-router-dom'
+} from "react-router-dom"
 // luum
-import { builtInTunerKit } from 'luum'
+import { builtInTunerKit } from "luum"
 // logic
-import manageHues from './logic/manageHues'
-import manageColors from './logic/manageColors'
+import manageHues from "./logic/manageHues"
+import manageColors from "./logic/manageColors"
 // Structure
-import {
-  DemoSelect,
-  ColorLogicDemo,
-} from './StyleDefinitions'
-import {
-  TilesDemo,
-} from './Tiles'
+import { DemoSelect, ColorLogicDemo } from "./StyleDefinitions"
+import { TilesDemo } from "./Tiles"
 // Children
-import PaletteModule from './PaletteModule'
+import PaletteModule from "./PaletteModule"
 // temporary
-import calibrationSheets from './preconfig/calibrationSheets'
-import useLocalStorageState from './util/useLocalStorageState'
-import HueModule from './HuesModule'
+import calibrationSheets from "./preconfig/calibrationSheets"
+import useLocalStorageState from "./util/useLocalStorageState"
+import HueModule from "./HuesModule"
 
 localStorage.clear()
 
 export default function Application() {
   const tuner = builtInTunerKit.simulateCMYK
-  const [hues, setHues] = useLocalStorageState('hues', calibrationSheets.default.hues)
-  const [colors, setColors] = useLocalStorageState('colors', calibrationSheets.default.colors)
+  const [hues, setHues] = useLocalStorageState(
+    "hues",
+    calibrationSheets.default.hues
+  )
+  const [colors, setColors] = useLocalStorageState(
+    "colors",
+    calibrationSheets.default.colors
+  )
 
   const [
     changeColor,
@@ -39,34 +40,30 @@ export default function Application() {
     redoChange,
     addColor,
     deleteColor,
-  ] = manageColors(
-    colors, setColors, tuner, hues
-  )
+  ] = manageColors(colors, setColors, tuner, hues)
 
-  const [
-    changeHue,
-    applyChangesToHues,
-    addHue,
-    deleteHue,
-  ] = manageHues(
-    hues, setHues, colors, applyChangesToColors
+  const [changeHue, applyChangesToHues, addHue, deleteHue] = manageHues(
+    hues,
+    setHues,
+    colors,
+    applyChangesToColors
   )
 
   return (
-    <Router>
+    <HashRouter>
       <DemoSelect>
         <ul>
-          <li><Link to='/'>Tiles</Link></li>
-          <li><Link to='/logic'>Logic</Link></li>
+          <li>
+            <Link to="/">Tiles</Link>
+          </li>
+          <li>
+            <Link to="/logic">Logic</Link>
+          </li>
         </ul>
       </DemoSelect>
       <Switch>
         <Route exact path="/">
-          <TilesDemo
-            colors={colors}
-            setColors={setColors}
-            tuner={tuner}
-          />
+          <TilesDemo colors={colors} setColors={setColors} tuner={tuner} />
         </Route>
         <Route path="/logic">
           <ColorLogicDemo>
@@ -78,7 +75,7 @@ export default function Application() {
               applyChangesToHues={applyChangesToHues}
               tuner={tuner}
             />
-            {[...colors, undefined].map((color, colorIdx) =>
+            {[...colors, undefined].map((color, colorIdx) => (
               <PaletteModule
                 key={`color-${colorIdx}`}
                 hues={hues}
@@ -91,11 +88,11 @@ export default function Application() {
                 undoChange={undoChange}
                 redoChange={redoChange}
               />
-            )}
+            ))}
           </ColorLogicDemo>
         </Route>
       </Switch>
-    </Router>
+    </HashRouter>
   )
 }
 // ${color ? color.hue : 0}-${color ? color.sat : 0}-${color ? color.lum : 0}
